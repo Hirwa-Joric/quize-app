7a8f0021124318d36e110cba9346d5de8479e339
Hirwa-Joric <122998728+Hirwa-Joric@users.noreply.github.com>
2023-05-08 08:33:09 +0200

    final commit

--- src/components/Aswer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Aswer = () => {
-  return (
-    <div>Aswer</div>
-  )
-}
-
-export default Aswer
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import Quiz from './components/Quiz';
-import { QuizProvider } from './context/Quiz';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Quiz from "./components/Quiz";
+import { QuizProvider } from "./contexts/quiz";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <QuizProvider>
       <Quiz />
     </QuizProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
